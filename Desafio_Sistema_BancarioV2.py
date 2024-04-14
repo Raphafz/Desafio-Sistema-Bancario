72573f70e22af5dec654a1a4cb488e6cfe79e099
Raphael Ferraz <raphael.ferraz21@gmail.com>
2024-04-14 12:46:04 -0300

    Versão 2 do desafio

---
+++ Desafio_Sistema_BancarioV2.py
@@ -0,0 +1,153 @@
+import textwrap
+
+banco = 'Bone Bank Corporation (BBC)'
+slogan = 'Seu dinheiro em boas mãos'
+
+def menu ():
+    print('-'*41)
+    print(f'{banco:^41}'.upper())
+    print(f'{slogan:^41}')
+    print('-'*41)
+    menu = '''\n
+             Seja bem-vindo ao BBC!
+    ================ MENU ===================
+    [1]\tDepositar
+    [2]\tSacar
+    [3]\tExtrato
+    [4]\tNovo usuário
+    [5]\tNova conta
+    [6]\tListar contas
+    [0]\tSair
+    Selecione uma operação: '''
+    return input(textwrap.dedent(menu)) 
+
+
+
+
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print('\n----- Valor depositado com sucesso!-----')
+
+    else:
+            print("\nOperação falhou! O valor informado é inválido.")
+    return saldo, extrato
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+
+    excedeu_saldo = valor > saldo
+
+    excedeu_limite = valor > limite
+
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("\nOperação falhou! Você não tem saldo suficiente.")
+
+    elif excedeu_limite:
+        print("\nOperação falhou! O valor do saque excede o limite.")
+
+    elif excedeu_saques:
+      print("\nOperação falhou! Número máximo de saques excedido.")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print('\n----- Saque realizado com sucesso!-----')
+        
+
+    else:
+        print("\nOperação falhou! O valor informado é inválido.")
+    return saldo, extrato
+
+def exibir_extrato(saldo,/, *, extrato ):
+    print("\n---------------- EXTRATO ----------------")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo:\t\t R$ {saldo:.2f}")
+    print("------------------------------------------")
+
+def criar_usuario(usuarios):
+    cpf= input('Digite o seu CPF (somente números): ')
+    usuario= filtrar_usuarios(cpf, usuarios)
+    if usuario:
+        print('\nJá existe um usuario com esse CPF!')
+        return
+    nome = input('Digite seu nome completo: ')
+    data_nascimento = input('Digite sua data de nascimento (dd/mm/aa): ')
+    edereco = input('Digite o seu endereço(logradouro, numero-bairro-cidade/sigla estado): ')
+    usuarios.append({"nome": nome, "data_nascimeto": data_nascimento, "cpf": cpf, "endereco": edereco})
+    print('\n---- Usuario criado com sucesso!----')
+
+def filtrar_usuarios(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Digite o CPF do usuário: ")
+    usuario = filtrar_usuarios(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\nUsuário não encontrado, criação de conta encerrada!")
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = '0001'
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    while True:
+    
+        opcao = menu()
+        
+        if opcao == '1':
+            valor = float(input('Informe o valor do depósito: '))
+            saldo, extrato = depositar(saldo, valor, extrato)
+
+        elif opcao == '2':
+            valor = float(input('Informe o valor do saque: '))
+
+            saldo, extrato = sacar(
+                saldo = saldo,
+                valor = valor,
+                extrato = extrato,
+                limite = limite,
+                numero_saques = numero_saques,
+                limite_saques = LIMITE_SAQUES,
+            )
+        elif opcao =='3':
+            exibir_extrato(saldo, extrato = extrato)
+        elif opcao == '4':
+            criar_usuario(usuarios)
+        elif opcao == '5':
+            numero_conta = len (contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+            if conta:
+                contas.append(conta)
+        elif opcao =='6':
+            listar_contas(contas)
+        elif opcao == '0':
+            break
+        else:
+            print('\nOperação inválida, por favor selecione uma operação correta!')
+
+main()
--- README.md
+++ README.md
@@ -9,6 +9,8 @@ O tema é "Criando um Sistema Bancário com Python". Onde a intenção do códig
 ## Tecnologias
 ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
 
+## Versão 1
+
 ### Operação de depósito
 Deve ser possível depositar valores positivos para uma conta bancária. A v1 do projeto trabalha com apenas 1 usuário, então não é necessário se preocupar com a identificação do número da agência e nem da conta bancária.
 
@@ -23,3 +25,7 @@ Todos os saques devem ser armazenados numa variável e exibidos na operação de
 Deve listar todos os depósitos e saques realizados na conta. No fim da lista, deve ser exibido o saldo atual da conta.
 
 Os valores devem ser exibidos utilizando o formato R$ xxx.xx.
+
+## Versão 2
+O objetivo é aprimorar a estrutura e a eficiência do sistema, implementando as operações de depósito, saque e extrato em funções específicas. Dividindo-o em funções reutilizáveis, facilitando a manutenção e o entendimento do sistema como um todo.
+Vamos criar funçoes para as operações extrato, sacar, deposito e visualizar histórico. Além disso, para a versão 2 do nosso sistema vamos criar duas novas funções: criar usuário (cliente do banco) e cria conta corrente (vincular com usuário).
